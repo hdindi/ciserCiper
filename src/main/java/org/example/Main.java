a5da5625b909c040d03807ad27cfd6e2dbdf7db0
Dindi Harris Samuel <harrisdindisamuel@gmail.com>
2023-08-20 10:19:48 -0700

    addition of string builder to help shift through the values

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,10 @@
 package org.example;
 
+import java.util.Scanner;
+
+// Press Ctrl+Alt+L to format the code.
+// Press Ctrl+Alt+O to optimize imports.
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
@@ -15,5 +20,28 @@ public class Main {
             // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
         }
+    }
+
+
+    public static String encrypt(String text, int shift) {
+        // Check if the text is an empty String or null and return it if true.
+        if (text == null || text.isEmpty() || shift <= 0) {
+            return text;
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        for (char c : text.toCharArray()) {
+            if (Character.isLetter(c)) {
+                int start = Character.isUpperCase(c) ? 'A' : 'a';
+                result.append((char) ((c - start + shift) % 26 + start));
+            } else {
+                result.append(c);
+            }
+        }
+
+
+
+        return result.toString();
     }
 }
